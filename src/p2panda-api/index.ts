ef842b087e70f2f9c637488f12eebd6126fdda28
cafca <cafca@001.land>
2021-05-14 02:40:51 +0200

    Add p2panda-api

---
+++ src/p2panda-api/index.ts
@@ -0,0 +1,8 @@
+import debug from "debug";
+
+export const log = debug("p2panda-api");
+
+export type Resolved<T> = T extends PromiseLike<infer U> ? Resolved<U> : T;
+
+export { default as Session } from "./session.js";
+export { default as Instance } from "./instance.js";
