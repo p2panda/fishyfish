3522a8efb92d7cc88c6858221fc92558b596b5ab
cafca <cafca@001.land>
2021-05-14 14:14:48 +0200

    Reset version

--- src/fishy.ts
+++ src/fishy.ts
@@ -3,7 +3,7 @@ import program from "commander";
 
 async function main() {
   program
-    .version("1.0.0")
+    .version("0.0.1")
     .description("A command line interface for p2panda nodes")
     .command("query [selector]", "query entries")
     .command("schema [command]", "create and list schemas");
