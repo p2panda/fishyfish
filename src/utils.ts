0adf96e4ee14babcd5a45c9e79653974c65ceec6
Vincent Ahrend <mail@vincentahrend.com>
2022-01-27 13:45:51 +0100

    Fix private key storage

--- src/utils.ts
+++ src/utils.ts
@@ -13,7 +13,11 @@ export const getKeyPair = async () => {
   try {
     fs.statSync(dataDir);
   } catch {
+    try {
     fs.mkdirSync(dataDir);
+    } catch(err) {
+      throw new Error(`Error creating application data directory at ${dataDir}: ${err}`)
+    }
   }
 
   const fname = path.join(dataDir, "secret.key");
@@ -21,13 +25,16 @@ export const getKeyPair = async () => {
   let privateKey = null;
   try {
     privateKey = fs.readFileSync(fname).toString();
-  } catch {}
+  } catch {
+    // No private key was found, proceeding to create one and write it to disk
+  }
 
   if (!privateKey) {
     console.log(chalk.green("New signing key created"), chalk.grey(fname));
-    const keyPair = p2panda.createKeyPair();
-    privateKey = keyPair.privateKey;
-    fs.writeFileSync(fname, privateKey);
+    const keyPair = await p2panda.createKeyPair();
+    privateKey = keyPair.privateKey();
+    // Only allow current user to access this file by setting mode 0700
+    fs.writeFileSync(fname, privateKey, { mode: 0o700 });
     return keyPair;
   }
 
